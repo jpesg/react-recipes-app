fef45890fa253da8e870ce62603dd5931518b362
jpesg <jpesgil@gmail.com>
2019-04-23 11:54:30 +0100

    navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,28 @@
 import React from "react";
+import logo from "../images/logo.svg";
+
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  return <div>hello from navbar</div>;
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-ligth">
+      <Link to="/" className="navbar-brand">
+        <img src={logo} alt="logo" />
+      </Link>
+      <div className="collapse navbar-collapse show ml-sm-5">
+        <ul className="navbar-nav">
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/recipes" className="nav-link">
+              Recipes
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
