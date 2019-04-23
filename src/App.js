951bcea5c72bcb7236fba9a54669bdc3d89507e9
jpesg <jpesgil@gmail.com>
2019-04-23 11:13:58 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <div>hello from recipes</div>;
+}
+
+export default App;
