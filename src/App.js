a4034f6d3f3e025875f798dfceaff582f76134c6
jpesg <jpesgil@gmail.com>
2019-04-23 11:38:18 +0100

    folder structure

--- src/App.js
+++ src/App.js
@@ -3,8 +3,29 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import Default from "./pages/Default";
+import SingleRecipe from "./pages/SingleRecipe";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div>hello from recipes</div>;
+  return (
+    <Router>
+      <main>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/recipes" exact component={Recipes} />
+          <Route path="/recipes/:id" component={SingleRecipe} />
+          <Route component={Default} />
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/SingleRecipe.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class SingleRecipe extends Component {
+  render() {
+    return <h4>single reipe page</h4>;
+  }
+}
