b379b7470d0f2dfc3d40e45570ab6e58018c66f2
jpesg <jpesgil@gmail.com>
2019-04-24 10:27:00 +0100

    Recipe Component, they are the items for the recipe list

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,48 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Recipe extends Component {
   render() {
-    return <div>recipe single</div>;
+    const {
+      image_url,
+      title,
+      source_url,
+      publisher,
+      recipe_id
+    } = this.props.recipe;
+    return (
+      <div className="col-10 mx-auto col-md-6 col-lg-4 my-3">
+        <div className="card" style={{ height: "100%" }}>
+          <img
+            className="card-img-top"
+            src={image_url}
+            alt=""
+            style={{ height: "14rem" }}
+          />
+          <div className="card-body text-capitalize">
+            <h6 className="card-title">{title}</h6>
+            <h6 className="text-warning text-slanted">
+              provider by {publisher}
+            </h6>
+          </div>
+          <div className="card-footer">
+            <Link
+              to={`/recipes/${recipe_id}`}
+              className="btn btn-primary text-capitalize"
+            >
+              details
+            </Link>
+            <a
+              href={source_url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn-success mx-2 text-capitalize"
+            >
+              recipe url
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
